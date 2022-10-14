afae5b81d20c673624d425140e85fd7f59e233f1
Sidret <sidretmangan@gmail.com>
2022-10-14 23:07:20 +0300

    IntVariable ScriptableObject added

---
+++ Assets/_Project/Scripts/Data/IntVariable.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace DrawCrusher.Database
+{
+    [CreateAssetMenu]
+    public class IntVariable : ScriptableObject
+    {
+#if UNITY_EDITOR
+        [Multiline]
+        public string DeveloperDescription = "";
+#endif
+        public int Value;
+        public void SetValue(int value)
+        {
+            Value = value;
+        }
+        public void SetValue(IntVariable value)
+        {
+            Value = value.Value;
+        }
+        public void ApplyChange(int amount)
+        {
+            Value += amount;
+        }
+        public void ApplyChange(IntVariable amount)
+        {
+            Value += amount.Value;
+        }
+    }
+}

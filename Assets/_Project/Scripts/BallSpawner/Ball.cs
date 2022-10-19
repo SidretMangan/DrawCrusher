e8489c3ba655e31101ab99b08200db0a43123ab1
Sidret <sidretmangan@gmail.com>
2022-10-19 03:32:00 +0300

    Ball Spawner updated optimized

--- Assets/_Project/Scripts/BallSpawner/Ball.cs
+++ Assets/_Project/Scripts/BallSpawner/Ball.cs
@@ -2,14 +2,14 @@ using System;
 using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
-using DG.Tweening;
 using DrawCrusher.BlockManagement;
+using Cysharp.Threading.Tasks;
 
 namespace DrawCrusher.BallSpawner
 {
     public class Ball : MonoBehaviour
     {
-        [SerializeField]private Rigidbody2D _rigidbody;
+        [SerializeField] private Rigidbody2D _rigidbody;
         [SerializeField] private LayerMask _blockLayerMask;
         [SerializeField] private float _explosionRadius;
         private readonly Subject<Unit> _finishedSubject = new Subject<Unit>();
@@ -27,16 +27,28 @@ namespace DrawCrusher.BallSpawner
         /// <summary>
         /// Initialize Ball
         /// </summary>
-        public void Initialize(Vector3 initPosition, Vector3 velocity)
+        public void Initialize(Vector3 initPosition)
+        {
+            transform.position = initPosition;
+            // end after 3.5f seconds
+            Observable.Timer(TimeSpan.FromSeconds(3.5))
+                .TakeUntilDisable(this)
+                .TakeUntilDestroy(this)
+                .Subscribe(_ => Finish());
+        }
+        /// <summary>
+        /// Initialize Ball
+        /// </summary>
+        public void InitializeWithVelocity(Vector3 initPosition, Vector3 velocity)
         {
             transform.position = initPosition;
 
-            //Observable.NextFrame(FrameCountType.FixedUpdate)
-            //    .TakeUntilDisable(this)
-            //    .Subscribe(_ => _rigidbody.AddForce(velocity, ForceMode2D.Force))
-            //    .AddTo(this);
+            Observable.NextFrame(FrameCountType.FixedUpdate)
+                .TakeUntilDisable(this)
+                .Subscribe(_ => _rigidbody.AddForce(velocity, ForceMode2D.Force))
+                .AddTo(this);
 
-            // end after 3.5f seconds
+            // end after 3.5 seconds
             Observable.Timer(TimeSpan.FromSeconds(3.5))
                 .TakeUntilDisable(this)
                 .TakeUntilDestroy(this)
@@ -44,15 +56,16 @@ namespace DrawCrusher.BallSpawner
         }
         private void BallOnHit()
         {
-            GetExplosionBlocks();
+            GetExplosionBlocks().Forget();
         }
-        private void GetExplosionBlocks()
+        private async UniTaskVoid GetExplosionBlocks()
         {
             Collider2D[] collidedBlocks = Physics2D.OverlapCircleAll(transform.position, _explosionRadius, _blockLayerMask);
             if (collidedBlocks.Length == 0) return;
             foreach (var block in collidedBlocks)
             {
-                block.gameObject.GetComponent<Block>().InvokeHit();
+                block.gameObject.GetComponent<Block>()?.InvokeHit();
+                await UniTask.NextFrame();
             }
             Finish();
         }
--- Assets/_Project/Scripts/BallSpawner/BallSpawnPoint.cs
+++ Assets/_Project/Scripts/BallSpawner/BallSpawnPoint.cs
@@ -11,9 +11,11 @@ namespace DrawCrusher.BallSpawner
         /// </summary>
         [SerializeField]
         private BallObjectPoolProvider _ballObjectPoolProvider;
-        [SerializeField] private float _spawnTime=1f;
+        [SerializeField] private float _spawnTime=1.5f;
+        [Space]
+        [Header("With Velocity")]
+        [SerializeField] private bool _withVelocity=false;
         [SerializeField] private float _speed=3f;
-
         private BallObjectPool _objectPool;
 
         private void Start()
@@ -32,9 +34,17 @@ namespace DrawCrusher.BallSpawner
 
             // ball placement
             var initPos = transform.position;
-            Vector3 dir = Vector3.up * -_speed;
-            // Set the initial position and velocity of the ball
-            b.Initialize(initPos, dir);
+            if (!_withVelocity)
+            {
+                // Set the initial position
+                b.Initialize(initPos);
+            }
+            else
+            {
+                // Set the initial position and velocity of the ball
+                Vector3 dir = Vector3.up * -_speed;
+                b.InitializeWithVelocity(initPos, dir);
+            }
 
             // Fires a ball and returns it to the ObjectPoo"l when finished
             b.OnFinishedAsync
